11f8f1a1368ccd1bfe172f3d33b7099ec655b69b
Khalis <khalis.qamarul@bukalapak.com>
2020-05-17 02:11:36 +0700

    update pingtest-5

--- pingtest-5.py
+++ pingtest-5.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import subprocess
 import ipaddress
+
+
 ping_result = open("ping-result.txt" , "w")
 net_addr = input("Enter Your IP with CIDR (ex: 192.168.10.1/24) : " )
 ip_addr = ipaddress.ip_network(net_addr, strict=False)
 all_host = list(ip_addr.hosts())
 
 for ip in all_host:
-    output = subprocess.Popen(["ping" , "-c 1" , str(ip)], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    output = output.communicate()[0]
+    #output = subprocess.Popen(["ping" , "-c 1" , str(ip)], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    output = subprocess.Popen(["ping" , "-c 1" , str(ip)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = output.communicate()
     #print(output)
-    if output == 0 in output:
-        print(f"{ip} is Online")
+    if (output.returncode == 0):
+        print(f"{ip} is UP")
+        ping_result.writelines(f"{ip} is UP \n")
     else:
-        print(f"{ip} is Offline")
-    
+        print(f"{ip} is DOWN")
+        ping_result.writelines(f"{ip} is DOWN \n")
+ 
